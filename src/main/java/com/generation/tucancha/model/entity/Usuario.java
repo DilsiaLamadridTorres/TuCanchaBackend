186f92a523e54f05ddfc44810aa07db86732c2ae
SebasRincon10 <sebasrincon.10@hotmail.com>
2026-09-08 15:34:19 -0500

    usuarios

--- src/main/java/com/generation/tucancha/model/entity/Usuario.java
+++ src/main/java/com/generation/tucancha/model/entity/Usuario.java
@@ -1,7 +1,103 @@
 package com.generation.tucancha.model.entity;
 
-/**
- * Entity placeholder for Usuario.
- */
-public class Usuario {
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+    @Table(name = "usuarios")
+    public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String nombre;
+    private String cedula;
+    private String correo;
+    private String password;
+    private boolean estado;
+    @Column(name = "fecha_registro", nullable = false, updatable = false)
+    private LocalDateTime fechaRegistro = LocalDateTime.now();
+    @ManyToMany(fetch=FetchType.LAZY)
+    @JoinTable(
+    name = "usuario_rol",
+    joinColumns = @JoinColumn(name = "usuario_id"),
+    inverseJoinColumns = @JoinColumn(name = "rol_id")
+)
+private List<Rol> roles;
+
+    public Usuario() {
+    }
+    public Usuario(String nombre, String cedula, String correo, String password, boolean estado) {
+        setNombre(nombre);
+        setCedula(cedula);
+        setCorreo(correo);
+        setPassword(password);
+        setEstado(estado);
+    }
+
+    public List<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Rol> roles) {
+        this.roles = roles;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
