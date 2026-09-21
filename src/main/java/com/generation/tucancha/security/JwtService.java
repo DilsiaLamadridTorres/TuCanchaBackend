1df46c6a3aa7a308458f0dfb8b92bcaef1d74fd1
SebasRincon10 <sebasrincon.10@hotmail.com>
2026-09-21 14:31:00 -0500

    fix4

--- src/main/java/com/generation/tucancha/security/JwtService.java
+++ src/main/java/com/generation/tucancha/security/JwtService.java
@@ -13,8 +13,8 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-//    @Value("${jwt.secret}")
-    private String jwtSecret="clave-secreta-larga-de-al-menos-32-caracteres-vetcare-2026";
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
     private static final long EXPIRACION_MS = 1000L * 60 * 60 * 8;
 
